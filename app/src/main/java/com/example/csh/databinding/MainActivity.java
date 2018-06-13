d682385b872168357afdf07e2c7ec2a69667e5e1
chengshenghao <1151799682@qq.com>
2018-06-13 18:07:41 +0800

    添加点击事件

--- app/src/main/java/com/example/csh/databinding/MainActivity.java
+++ app/src/main/java/com/example/csh/databinding/MainActivity.java
@@ -21,6 +21,11 @@ public class MainActivity extends AppCompatActivity {
         user.isShowPhone = true;
         // 绑定数据
         binding.setUser(user);
+        // 绑定方法和监听
+        MyHandler handler = new MyHandler();
+//        MyTask task = new MyTask(this);
+        binding.setHandler(handler);
+//        binding.setTask(task);
 
     }
 }
